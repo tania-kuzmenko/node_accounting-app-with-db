80310a4f424afc328005b3e196ba72c7d2352e6b
Tetiana Kuzmenko <tanch.sergeevich@gmail.com>
2026-06-07 18:32:26 +0200

    remove client part

--- client/src/routes copy/category.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as categoryController from '../controllers/category.controller.js';
-
-export const categoryRouter = express.Router();
-categoryRouter.get('/', categoryController.get);
-categoryRouter.post('/', categoryController.create);
-categoryRouter.get('/:id', categoryController.getOne);
-categoryRouter.patch('/:id', categoryController.update);
-categoryRouter.delete('/:id', categoryController.remove);
